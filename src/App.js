83f41916ea7b58e7f0e34c8491431f090392222f
Kevin Pessetto <kjpessetto@gmail.com>
2019-06-10 16:33:43 -0500

    initial commit media queries and hamberder menu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./reset.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      menuStatus: "drop-down-menu"
+    };
+  }
+
+  handleClick = () => {
+    if (this.state.menuStatus === "drop-down-menu-open") {
+      this.setState({ menuStatus: "drop-down-menu-closed" });
+    } else {
+      this.setState({ menuStatus: "drop-down-menu-open" });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <ul>
+            <div>
+              <li className="nav-title">Start Bootstrap</li>
+
+            </div>
+            <li className="menu-text">Services</li>
+            <li className="menu-text">Portfolio</li>
+            <li className="menu-text">About</li>
+            <li className="menu-text">Team</li>
+            <li className="menu-text">Contact</li>
+            <li>
+              <button onClick={this.handleClick} className="menu-button">
+                MENU
+                <img
+                  src="https://cdn4.iconfinder.com/data/icons/wirecons-free-vector-icons/32/menu-alt-512.png"
+                  alt="ham_menu"
+                />
+              </button>
+            </li>
+            <div className={this.state.menuStatus}>
+            <div className="bork">
+                <ul>
+                <button className="drop-down-button">Services</button>
+                <button className="drop-down-button">Portfolio</button>
+                <button className="drop-down-button">About</button>
+                <button className="drop-down-button">Team</button>
+                <button className="drop-down-button">Contact</button>
+                </ul>
+              </div>
+
+            </div>
+          </ul>
+        </nav>
+        <header className="header">
+          <h2 className="welcome">Welcome To Our Studio!</h2>
+          <h3 className="nice">IT'S NICE TO MEET YOU</h3>
+          <button className="button">TELL ME MORE</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
